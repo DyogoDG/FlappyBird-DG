480e497a99070b312bfd46f181d611a55d32238f
DyogoDG <DyogoDG@hotmail.com>
2024-03-18 21:25:24 -0300

    Destruição dos Canos após passar pela cena.

--- Assets/Scripts/Pipe.cs
+++ Assets/Scripts/Pipe.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Pipe : MonoBehaviour
 {
@@ -13,5 +14,12 @@ public class Pipe : MonoBehaviour
     void Update()
     {
         transform.position += Vector3.left * Time.deltaTime * speed;
+
+        if(transform.position.x < -1.743f)
+        {
+            //Auto Destruir
+            Destroy(gameObject);
+        }
+
     }
 }
